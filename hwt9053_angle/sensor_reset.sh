848618bf89cb6f66341a5aa70d9c0c35c011db87
fanrayk <s5069612719@gmail.com>
2025-02-21 10:27:06 +0800

    新增感測器歸零腳本

--- hwt9053_angle/sensor_reset.sh
+++ hwt9053_angle/sensor_reset.sh
@@ -1,4 +1,4 @@
 sudo pm2 stop tiltmeter
-sudo bash -c 'printf "\x50\x06\x00\x01\x00\x08\xD4\x4D" > /dev/ttyUSB0;'
-sudo bash -c 'printf "\x50\x06\x00\x01\x00\x04\xD4\x48" > /dev/ttyUSB0;'
+sudo bash -c 'stty -F /dev/ttyUSB0 115200 raw; printf "\x50\x06\x00\x00\x00\x00\x84\x4B" > /dev/ttyUSB0;'
+# sudo bash -c 'stty -F /dev/ttyUSB0 9600 cs8 -cstopb -parenb raw; printf "\x50\x03\x00\x3D\x00\x06\x59\x85" > /dev/ttyUSB0; sleep 1; timeout 1 dd if=/dev/ttyUSB0 bs=1 count=100 2>/dev/null | od -tx1 -An'
 sudo pm2 start tiltmeter

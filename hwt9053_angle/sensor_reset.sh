19dc988bf9b429a9d5a7571f0d2f30a5b8ac2318
fanrayk <s5069612719@gmail.com>
2025-03-04 14:00:53 +0800

    修改sensor的port

--- hwt9053_angle/sensor_reset.sh
+++ hwt9053_angle/sensor_reset.sh
@@ -1,4 +1,4 @@
 sudo pm2 stop tiltmeter
-sudo bash -c 'stty -F /dev/ttyUSB0 115200 raw; printf "\x50\x06\x00\x01\x00\x08\xD4\x4D" > /dev/ttyUSB0;'
-sudo bash -c 'stty -F /dev/ttyUSB0 115200 raw; printf "\x50\x06\x00\x01\x00\x04\xD4\x48" > /dev/ttyUSB0;'
+sudo bash -c 'stty -F /dev/ttyS0 115200 raw; printf "\x50\x06\x00\x01\x00\x08\xD4\x4D" > /dev/ttyUSB0;'
+sudo bash -c 'stty -F /dev/ttyS0 115200 raw; printf "\x50\x06\x00\x01\x00\x04\xD4\x48" > /dev/ttyUSB0;'
 sudo pm2 start tiltmeter

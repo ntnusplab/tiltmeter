994d186f32b5cdeab61f66d1b97cb0acbb6f3f5e
fanrayk <s5069612719@gmail.com>
2025-04-14 15:09:34 +0800

    調整mbim

--- mbim_start_connect.sh
+++ mbim_start_connect.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# 先停止之前背景執行的 waveshare-CM 程序
+# 停止可能已經存在的 waveshare-CM 進程
 pkill -f waveshare-CM
 
-# 從 apn.conf 中讀取 APN 變數
+# 從 sys.conf 中讀取 APN 變數
 source /home/admin/tiltmeter/sys.conf
 
 echo "正在使用 APN: $APN 啟動 4G 連線..."
-sudo waveshare-CM -s "$APN" &
+
+# 直接執行 waveshare-CM（前景模式）
+exec waveshare-CM -s "$APN"
